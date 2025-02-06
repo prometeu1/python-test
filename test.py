0049e616cab65e9d94be65a46a0e85cc34f32a5d
prometeu1 <kevin.bajurean@oteria.fr>
2025-02-06 15:31:56 +0100

    exercice 3 v2

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+def column(*args):
+  for arg in args:
+    b = len(arg) + 4
+    c = "-" * b
+    d = "|"
+
+    print(c)
+    print(d, arg, d)
+    
+
+
+column("hello", "world")
+
 def line(a):
     b = len(a) + 4
     c = "-" * b
@@ -9,16 +22,3 @@ def line(a):
     print(c)
 
 line("oteria")
-
-def column(*args):
-  for arg in args:
-    b = len(arg) + 4
-    c = "-" * b
-    d = "|"
-
-    print(c)
-    print(d, arg, d)
-    print(c)
-
-
-column("hello", "world")
